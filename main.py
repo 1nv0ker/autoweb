1801bc056086587f4c0379cea9797e0526e51af6
FTP <760604256@qq.com>
2025-08-03 23:39:29 +0800

    1 Signed-off-by: FTP <760604256@qq.com>

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from DrissionPage import ChromiumPage, ChromiumOptions, SessionPage
+from DrissionPage import ChromiumPage, ChromiumOptions, SessionPage,WebPage
 from fake_useragent import UserAgent
 from user_agents import parse
 import random
+import requests
+import json
+
+headers = {'Content-Type': 'application/json'}
 # browser = Chromium() 
 # tab = browser.latest_tab
 # tab.get('https://www.iploong.com')
@@ -54,16 +58,29 @@ proxy.bitip.com:10004:1c7t3eykxutgrbitip_g-US_f-129029g9h9g1:127939ghg92g2f28'''
         proxies.append('https://'+data[2]+':'+data[3]+'@'+data[0]+':'+data[1])
     return proxies
 
+def create_bit_browser():
+    # browser_id = createBrowser()
+    url = "http://127.0.0.1:54345/browser/open"
+    headers = {'X-API-KEY': 'f2a39b9ffc0d4d21a2b4f6a637e1e8ed'}
+    json_data = {'id': 'f657877d57e145d6b6408720adb2dfa6'}
+    res = requests.post(url, json=json_data, headers=headers).json()
+    print(res)
+    return res['data']['driver']
+
 def main():
     deviceinfo = getRandom()
     proxies = getProxy()
     co = ChromiumOptions()
     print(deviceinfo)
+    # driverPath  = create_bit_browser()
+    co.set_argument('--disable-blink-features=AutomationControlled')
+    # co.set_argument('--no-sandbox')
+    # co.set_argument('--headless')
     co.set_user_agent(deviceinfo['userAgent'])
-    
+    # co.set_browser_path(driverPath)
     # co.set_argument('--disable-blink-features=AutomationControlled')
-    co.set_argument('--window-size', str(deviceinfo["width"])+','+str(deviceinfo["height"]))
-    co.add_extension('./extension')
+    # co.set_argument('--window-size', str(deviceinfo["width"])+','+str(deviceinfo["height"]))
+    # co.add_extension('./extension')
     # proxy = random.choice(proxies)
     # print(proxy)
     # co.set_proxy(proxy)
@@ -85,8 +102,10 @@ def main():
     #         return oldToDataURL.apply(this, arguments);
     #     };
     # """)
-
+    # print(WebPage)
+    
+    # page = WebPage(chromium_options=co)
     page.get('https://www.iploong.com')
-
+    # page.
 
 main()
