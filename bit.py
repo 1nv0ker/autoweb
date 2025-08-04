a1c33fd8ace0496f13b14e8110ed05a98c465b85
FTP <760604256@qq.com>
2025-08-04 23:25:16 +0800

    1 Signed-off-by: FTP <760604256@qq.com>

--- bit.py
+++ bit.py
@@ -1,14 +1,19 @@
 from DrissionPage import ChromiumOptions,Chromium
 from bitApi import createBrowser,openBrowser
-
+from plugins import generate_random_string
 def main():
-    co1 = ChromiumOptions().set_local_port(9222).set_user_data_path('data3')
+    filename = generate_random_string(8)
+    filename = 'data2'
+    co1 = ChromiumOptions().set_local_port(9222).set_user_data_path('webData/'+filename)
     # co2 = ChromiumOptions().set_local_port(9333).set_user_data_path('data2')
     co1.add_extension('./extension')
     # co2.add_extension('./extension')
     browser1 = Chromium(co1)
     # browser2 = Chromium(co2)
-    browser1.new_tab('https://iploong.com')
+    tab = browser1.new_tab('https://www.iploong.com/')
+    plugin_selector = 'div[aria-label="Similarweb - Website Traffic & SEO Checker"]'
+    test = tab.ele('div[aria-label="Similarweb - Website Traffic & SEO Checker"]')
+    # print(test)
     # browser2.new_tab('https://iploong.com')
     # browserId = createBrowser()
     # driverPath = openBrowser(browserId)
---
+++ plugins.py
@@ -0,0 +1,20 @@
+import random
+import string
+
+def generate_random_string(length: int) -> str:
+    """
+    生成指定长度的随机字母数字混合字符串
+    
+    参数:
+        length: 生成的字符串长度
+        
+    返回:
+        随机字母数字混合字符串
+    """
+    # 定义字符集：包含大小写字母和数字
+    characters = string.ascii_letters + string.digits
+    
+    # 从字符集中随机选择字符，组成指定长度的字符串
+    random_string = ''.join(random.choice(characters) for _ in range(length))
+    
+    return random_string
